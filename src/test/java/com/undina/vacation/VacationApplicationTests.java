23ca37df1836e459002fb98943901e6e70ad49e3
undina <foxundina@gmail.com>
2023-05-11 16:04:05 +0300

    Initial commit

---
+++ src/test/java/com/undina/vacation/VacationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undina.vacation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
